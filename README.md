9ead9997a116d5c104ffa7c3fefccc6933020a17
Arrya-A <arrya98@gmail.com>
2025-04-06 14:13:22 +0530

    completed

--- README.md
+++ README.md
@@ -1,36 +1,34 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+Profile Creator - Next.js Project
 
-## Getting Started
+The application will allow users to input their profile details and validate the password based on specified policies
 
-First, run the development server:
+# Features
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
+- Collects user profile data: Name, Email, Phone (optional), Password
+- Validates fields:
+    - Required: Name, Email, Password
+    - Email must be a valid email address format
+    - Phone must be numeric if provided
+    - Password must:
+        - Must be at least 8 characters long
+        - Must contain at least one uppercase letter (A-Z)
+        - Must contain at least one lowercase letter (a-z)
+        - Must include at least one numeric digit (0-9)
+        - Must include at least one special character (e.g., @, #, $, %, etc.)
+- Validation and Error Messages
+- Responsive UI
+- Confirmation message on successful submission
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
-You can start editing the page by modifying `app/page.js`. The page auto-updates as you edit the file.
 
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
+# To run
+navigate to project file using command,
+    cd profile_creator
 
-## Learn More
+install using command,
+    npm install
 
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+run using the command,
+    npm run dev
+    
+navigate to http://localhost:3000 in your browser
--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,114 @@
-import Image from "next/image";
+'use client'
 
-export default function Home() {
+import { useState } from "react"
+
+export default function FormPage() {
+  const [formData, setFormData] = useState({
+    name: '', email: '', phone: '', password: ''
+  })
+
+  const [errors, setErrors] = useState("")
+  const [submitted, setSubmitted] = useState(false)
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+    setErrors({ ...errors, [e.target.name]: '' })
+  }
+
+  const validateEmail = (email) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+    return regex.test(email)
+  }
+
+  const validatePassword = (password) => {
+    const errorMessages = [];
+    if (password.length < 8) errorMessages.push('At least 8 characters');
+    if (!/[A-Z]/.test(password)) errorMessages.push('One uppercase letter');
+    if (!/[a-z]/.test(password)) errorMessages.push('One Lowercase letter');
+    if (!/[0-9]/.test(password)) errorMessages.push('One number');
+    if (!/[!@#$%^&*(),.?":{}|<>]/.test(password)) errorMessages.push('One special character');
+    return errorMessages;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    const newErrors = {}
+    setSubmitted(false)
+
+    if (!formData.name.trim()) newErrors.name = 'Name is required'
+    if (!formData.email.trim()) newErrors.email = 'Email is required'
+    else if (!validateEmail(formData.email)) newErrors.email = 'Invalid email'
+
+    if (!formData.password) {
+      newErrors.password = 'Password is required'
+    } else {
+      const passwordIssues = validatePassword(formData.password)
+      if (passwordIssues.length > 0) {
+        newErrors.password = `Password must include: ${passwordIssues.join(', ')}`
+      }
+    }
+
+    if (formData.phone && isNaN(formData.phone)) {
+      newErrors.phone = 'Phone must be numberic'
+    }
+
+    setErrors(newErrors)
+
+    if (Object.keys(newErrors).length === 0) {
+      setSubmitted(true)
+    }
+  }
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="min-h-screen flex items-center justify-center px-4 bg-linear-to-r from-blue-100 to-green-100" >
+      <div className="w-100  bg-white p-6 rounded-lg shadow">
+        <h1 className="text-2xl text-lime-700  mb-6 text-center">User Form</h1>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <div>
+            <input type="text" name="name" placeholder='Name' className="mt-1 block w-full border rounded p-2" value={formData.name} onChange={handleChange} />
+            {errors.name && <p className="text-red-500 text-sm">{errors.name}</p>}
+          </div>
+
+          <div>
+            <input type="email" name="email" placeholder='Email' className="mt-1 block w-full border rounded p-2" value={formData.email} onChange={handleChange} />
+            {errors.email && <p className="text-red-500 text-sm">{errors.email}</p>}
+          </div>
+
+          <div>
+            <input type="text" name="phone" placeholder='Phone (optional)' className="mt-1 block w-full border rounded p-2" value={formData.phone} onChange={handleChange} />
+            {errors.phone && <p className="text-red-500 text-sm">{errors.phone}</p>}
+          </div>
+
+          <div>
+            <input type="password" name="password" placeholder='Password' className="mt-1 block w-full border rounded p-2" value={formData.password} onChange={handleChange} />
+            {errors.password && <p className="text-red-500 text-sm">{errors.password}</p>}
+          </div>
+
+          <button type="submit" className="w-full bg-lime-700 text-white py-2 rounded hover:bg-lime-800" >
+            SUBMIT
+          </button>
+        </form>
+
+        <div className='my-3'>
+          {submitted &&
+            (
+              <div className="flex items-center justify-center my-3 bg-green-100 text-green-800 p-4 rounded mb-4 text-center">
+                <div className='mr-2'><i className="fa-regular fa-circle-user mr-2"></i> Profile successfully created!</div>
+                <button onClick={() => {
+                  setFormData({ name: '', email: '', phone: '', password: '' })
+                  setErrors({})
+                  setSubmitted(false)
+                }}
+                  className="mt-2 px-2 py-1 bg-lime-700 text-white rounded-lg hover:bg-lime-800"
+                >
+                  OK
+                </button>
+              </div>
+            )
+          }
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
-  );
+  )
 }
